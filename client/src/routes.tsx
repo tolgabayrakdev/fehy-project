3b6e51b4982f0730fe939c638768b1e849ae6c4e
tolgabayrakdev <bayraktolga28@gmail.com>
2023-07-29 16:59:00 +0300

    react wtih vite typescirpt

---
+++ client/src/routes.tsx
@@ -0,0 +1,13 @@
+import { useRoutes } from "react-router-dom";
+
+
+
+export default function Router(){
+    const routes = useRoutes([
+        {
+            path: "/",
+            
+        }
+    ])
+    return routes;
+}
