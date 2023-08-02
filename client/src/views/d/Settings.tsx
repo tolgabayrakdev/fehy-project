396fcba78a630a29d61e976119e507957b7402e1
tolgabayrakdev <bayraktolga28@gmail.com>
2023-08-02 17:19:58 +0300

    settings page tabs button

--- client/src/views/d/Settings.tsx
+++ client/src/views/d/Settings.tsx
@@ -6,37 +6,43 @@ type Props = {}
 export default function Settings({ }: Props) {
   return (
     <div>
-      <h3 className="text-lg mt-4">Update your account information.</h3>
+      <h3 className="text-lg mt-4">Update your account information and see favs.</h3>
       <hr className="mt-3 mb-9" />
       <Tabs isFitted variant="enclosed">
         <TabList className="flex flex-row">
-          <Tab>One</Tab>
+          <Tab>Favs</Tab>
           <Tab>Change username</Tab>
           <Tab>Change password</Tab>
         </TabList>
-
         <TabPanels>
           <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
+            <p>Favs quotes!</p>
           </TabPanel>
           <TabPanel>
             <div className="mt-12">
-            <p>Change Password</p>
-
+              <p>Current username: Tolga123</p>
               <form className="mt-5">
-                <Input placeholder="Enter current password" className="block" w="96" />
-                <Input placeholder="Enter new password" className="block mt-4" w="96" />
-                <Input placeholder="Enter verify new password" className="block mt-4" w="96" />
+                <Input placeholder="Enter new username" className="block" w="96" />
 
-                <Button className="mt-4">
+                <Button className="mt-4" color="green.300" variant="outline">
                   Save Changes
                 </Button>
               </form>
 
 
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="mt-12">
+              <form className="mt-5">
+                <Input placeholder="Enter current password" className="block" w="96" />
+                <Input placeholder="Enter new password" className="block mt-4" w="96" />
+                <Input placeholder="Enter verify new password" className="block mt-4" w="96" />
+
+                <Button className="mt-4" color="green.300" variant="outline">
+                  Save Changes
+                </Button>
+              </form>
             </div>
           </TabPanel>
         </TabPanels>
