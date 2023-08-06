f062cf0c33ca8c70204ca9b8eb8051c2978da7ad
tolgabayrakdev <bayraktolga28@gmail.com>
2023-08-06 09:29:31 +0300

    discover

--- client/src/layouts/QuotesLayout.tsx
+++ client/src/layouts/QuotesLayout.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet, useLocation, useParams } from "react-router-dom"
+import { Link, Outlet, useLocation } from "react-router-dom"
 
 
 
-export default function QuotesLayout(pathname: any) {
+export default function QuotesLayout() {
     const location = useLocation();
     const [pathnames, setPathnames] = useState(location.pathname);
     console.log(window.location.pathname);
@@ -13,24 +13,36 @@ export default function QuotesLayout(pathname: any) {
         setPathnames(location.pathname);
     }, [location.pathname]);
 
-    
-    return (
-        <div>
-            <h1 className="text-2xl font-medium">Hi, Tolga</h1>
-            <p className="mt-1">Lets explore quotes</p>
 
-            <div className="flex mt-8">
-                <div className="w-2/12 bg-gray-100">
+    return (
+        <div className="flex">
+            <div className="w-2/12">
+                <div className="flex flex-wrap mb-5">
+                    <h1 className="text-xl font-medium">Hi, Tolga</h1>
+                    <button className="border p-1 pr-3 pl-3 rounded-lg ml-4 bg-green-400 text-white hover:bg-green-500 duration-300">New Quote</button>
+                    <p className="mt-1 w-full">Lets explore quotes</p>
+                </div>
+
+
+                <div className="bg-gray-100 h-36 mt-4 fixed w-52 rounded-md p-1">
                     <h4 className="text-center text-xl">Yan menu</h4>
                     <hr />
-                    <Link to="/d" className={`${pathnames === "/d" ? "bg-blue-600 text-white flex": "text-black"} justify-center mt-5 flex mb-1 hover:bg-blue-600 hover:cursor-pointer duration-300 hover:text-white p-1 rounded-md`}>Quotes</Link>
-                    <Link to="/d/best-favs" className="flex justify-center mt-2 mb-1 hover:bg-blue-600 hover:cursor-pointer duration-300 p-1 hover:text-white rounded-md">Best favs</Link>
+                    <Link to="/d" className={`${pathnames === "/d" ? "bg-blue-600 text-white flex" : "text-black"} justify-center mt-5 flex mb-1 hover:bg-blue-600 hover:cursor-pointer duration-300 hover:text-white p-1 rounded-md`}>Quotes</Link>
+                    <Link to="/d/best-favs" className={`${pathnames === "/d/best-favs" ? "bg-blue-600 text-white flex" : "text-black"} flex justify-center mt-2 mb-1 hover:bg-blue-600 hover:cursor-pointer duration-300 p-1 hover:text-white rounded-md`}>Best favs</Link>
                 </div>
-                <div className="w-10/12 bg-gray-100 ml-4">
-                    <div>
-                        <Outlet />
-                    </div>
+            </div>
+
+            <div className="mt-24 rounded-sm w-7/12">
+                <div className=" bg-gray-50 ml-4 pt-12">
+                    <Outlet />
                 </div>
+            </div>
+            <div className="mt-24 pl-6 rounded-sm w-3/12">
+                <div className="bg-gray-100 h-36 fixed w-60 rounded-md">
+                    <h4>Diğer menu</h4>
+                </div>
+               
+
             </div>
         </div>
     )
--- client/src/views/d/Discover.tsx
+++ client/src/views/d/Discover.tsx
@@ -1,10 +1,80 @@
-type Props = {}
+import { Divider } from "@chakra-ui/react";
+import { useEffect } from "react"
+export default function Discover() {
+  useEffect(() => {
 
-export default function Discover({ }: Props) {
+
+  }, [])
+
+
+
   return (
-    <div>
-    burala quoters
-    
+    <div className="p-1 m-1 flex flex-wrap gap-3 ml-8">
+      <div className="grid gap-x-8 mr-6 gap-y-4 grid-cols-1 w-full">
+        <div className="border h-18 p- rounded-md">
+          <h4 className="mb-12 text-center mt-1 font-serif">''  Alıntı ''</h4>
+          <Divider />
+          <div className="h-16 ml-3">
+            <div>
+            <p className="text-xs pt-4">Tolga Bayrak</p>
+            <p className="text-xs">3-08-2023 Tuesday</p>
+            </div>
+            <p className="text-xs mr-3 flex justify-end text-red-600 hover:text-red-500 hover:cursor-pointer active:underline active:text-sm duration-300">Fav</p>
+
+          </div>
+        </div>
+       
+      </div>
+
+      <div className="grid gap-x-8 mr-6 gap-y-4 grid-cols-1 w-full">
+        <div className="border h-18 p- rounded-md">
+          <h4 className="mb-12 text-center mt-1 font-serif">''  Alıntı ''</h4>
+          <Divider />
+          <div className="h-16 ml-3">
+            <div>
+            <p className="text-xs pt-4">Tolga Bayrak</p>
+            <p className="text-xs">3-08-2023 Tuesday</p>
+            </div>
+            <p className="text-xs mr-3 flex justify-end text-red-600 hover:text-red-500 hover:cursor-pointer active:underline active:text-sm duration-300">Fav</p>
+
+          </div>
+        </div>
+       
+      </div>
+      <div className="grid gap-x-8 mr-6 gap-y-4 grid-cols-1 w-full">
+        <div className="border h-18 p- rounded-md">
+          <h4 className="mb-12 text-center mt-1 font-serif">''  Alıntı ''</h4>
+          <Divider />
+          <div className="h-16 ml-3">
+            <div>
+            <p className="text-xs pt-4">Tolga Bayrak</p>
+            <p className="text-xs">3-08-2023 Tuesday</p>
+            </div>
+            <p className="text-xs mr-3 flex justify-end text-red-600 hover:text-red-500 hover:cursor-pointer active:underline active:text-sm duration-300">Fav</p>
+
+          </div>
+        </div>
+       
+      </div>
+       <div className="grid gap-x-8 mr-6 gap-y-4 grid-cols-1 w-full">
+        <div className="border h-18 p- rounded-md">
+          <h4 className="mb-12 text-center mt-1 font-serif">''  Alıntı ''</h4>
+          <Divider />
+          <div className="h-16 ml-3">
+            <div>
+            <p className="text-xs pt-4">Tolga Bayrak</p>
+            <p className="text-xs">3-08-2023 Tuesday</p>
+            </div>
+            <p className="text-xs mr-3 flex justify-end text-red-600 hover:text-red-500 hover:cursor-pointer active:underline active:text-sm duration-300">Fav</p>
+
+          </div>
+        </div>
+       
+      </div>
+
+
+
+
     </div>
   )
 }
